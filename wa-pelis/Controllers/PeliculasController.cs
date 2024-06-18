552c7e6c8aa1900fe0cc9f889eab932fb41f5761
Carlos Bedoya <bedoyacarlosalberto@gmail.com>
2024-06-18 11:01:24 -0500

    primer commit

---
+++ wa-pelis/Controllers/PeliculasController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using wa_pelis.Contexto;
+using wa_pelis.Entitys;
+using wa_pelis.Interfaces;
+using wa_pelis.Models.BD;
+
+namespace wa_pelis.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PeliculasController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _ipelis;
+
+        public PeliculasController(ApplicationDbContext ipelis)
+        {
+            _ipelis = ipelis;
+        }
+
+
+        [HttpGet("consultarPeliculas")]
+        public async Task<IEnumerable<TblPelicula>> ConsultarPeliculas()
+        {
+            return _ipelis.TBL_Peliculas.ToList();
+        }
+            /*
+        public async List<TblPelicula> ConsultarPeliculas()
+        {
+            List<TblPelicula> listaPeliculas = new List<TblPelicula>();
+            using (var db = new Models.BD.DbPelisContext())
+            {
+                listaPeliculas = (from d in db.TblPeliculas
+                       select new TblPelicula
+                       {
+                           Id = d.Id,
+                           Nombre = d.Nombre,
+                           Genero = d.Genero
+                       }).ToList();
+            }
+            return listaPeliculas;
+
+        }
+            */
+    }
+}
